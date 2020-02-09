64e3dcb42bedc20c69c8858bb0e913a0cf1f0c09
Nick <ndlyga@syr.edu>
2020-02-09 13:36:10 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//Element nodes
+
+
+//Event Listeners
+
+
+
+//Variables for the game
+
+
+
+//Play a round of Rock, Paper, Scissors
+function playRound() {
+ 
+}
+
+
+//Update the text between the scores with the result of the round and with what each player played
+function resultText() {
+    
+}
+
+
+//Reset scores and text elements to 0
+function resetGame() {
+    
+}
+
+
+//Alert the player whether they won or not after someone reaches 10 points
+function gameOver() {
+    
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Paper, Rock, Scissors
+
+Building a simple game with JavaScript!
+
+## Rubric
+
+- (5pts) Buttons Work
+- (5pts) Scoring works
+- (5pts) All Text works
+- (5pts) Winning and Reset functionality works
